980c2d5d45acc0368f54bed7b9079a91e0b82761
김정현 <gimjeonghyeon@gimjeonghyeon-ui-MacBookPro.local>
2025-06-13 23:32:17 +0900

    feat : default layout 생성

---
+++ lib/_common/layout/view/detault_layout.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class DetaultLayout extends StatelessWidget {
+  final Widget child;
+  final String title;
+  final bool? isBackButtonEnabled;
+
+  const DetaultLayout({
+    super.key,
+    required this.child,
+    required this.title,
+    this.isBackButtonEnabled = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: isBackButtonEnabled ?? false,
+        centerTitle: false,
+        backgroundColor: Colors.black,
+        elevation: 10,
+        shadowColor: Colors.black,
+        title: Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: child,
+    );
+  }
+}

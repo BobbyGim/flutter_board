bedf6f920a7a16f7b2144c6f3952aafd4cb9e994
김정현 <gimjeonghyeon@gimjeonghyeon-ui-MacBookPro.local>
2025-06-13 23:33:04 +0900

    feat: home board 페이지 default layout 적용

--- lib/board/view/board_screen.dart
+++ lib/board/view/board_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_board/_common/layout/view/detault_layout.dart';
+import 'package:go_router/go_router.dart';
 
 class BoardScreen extends StatelessWidget {
   const BoardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Board Screen'));
+    return DetaultLayout(
+      title: '게시판',
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            context.pop();
+          },
+          child: const Text('로그인 화면으로 이동'),
+        ),
+      ),
+    );
   }
 }
--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_board/_common/layout/view/detault_layout.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Screen'));
+    return DetaultLayout(
+      title: '홈',
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            context.pop();
+          },
+          child: const Text('안녕 화면으로 이동'),
+        ),
+      ),
+    );
   }
 }
